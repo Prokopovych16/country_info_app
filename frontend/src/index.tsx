5162769a39e2c72534d910a4d6e34499a0d9fb1c
Maksym Prokopovych <prokopovych2006@gmail.com>
2024-12-03 10:41:06 +0200

    final-project-bc

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
-import { BrowserRouter as Router } from 'react-router-dom'; // Use BrowserRouter for routing
+import { BrowserRouter as Router } from 'react-router-dom';
 import React from 'react';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
